ef6ce11b3d1be723748380878855f4874c7b855b
James Ronel E. Andrade <espilajames@gmail.com>
2024-10-04 18:30:06 +0800

    Update success.php

--- success.php
+++ success.php
@@ -4,3 +4,13 @@
     <meta charset="UTF-8">
     <title>Success</title>
 </head>
+<body>
+    <h1>Success!</h1>
+    <p><?php echo htmlspecialchars($_GET['message']); ?></p>
+    <a href="exercise6.html">Go back</a>
+
+    <!-- Include the footer -->
+    <?php include 'footer.php'; ?>
+
+</body>
+</html>
